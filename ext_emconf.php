73ab0ed63b675c9a2ec9a89daa91c61b3aa39731
Jeff Segars <jeff@busynoggin.com>
2012-03-23 13:57:53 -0500

    Raise minimum TYPO3 version to 4.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.3.0-0.0.0',
+			'typo3' => '4.4.0-0.0.0',
 			'templavoila' => '1.5.0-0.0.0',
 		),
 		'conflicts' => array(
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,7 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-if (t3lib_div::int_from_ver(TYPO3_version) >= 4004000) {
-	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][] = 'EXT:templavoila_framework/class.tx_templavoilaframework_lib.php:&tx_templavoilaframework_lib->includeTypoScriptForFrameworkCoreAndSkins';
-	$backendStylesheet = 'backend.css';
-} else {
-	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_tstemplate.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'xclass/class.ux_t3lib_tstemplate.php';
-	$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_tsparser_ext.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'xclass/class.ux_t3lib_tsparser_ext.php';
-	$backendStylesheet = 'backend_typo3_4-3.css';
-}
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][] = 'EXT:templavoila_framework/class.tx_templavoilaframework_lib.php:&tx_templavoilaframework_lib->includeTypoScriptForFrameworkCoreAndSkins';
 
 // Update TemplaVoila with special stylesheet and Javascript
 t3lib_extMgm::addPageTSConfig('
@@ -16,7 +9,7 @@ t3lib_extMgm::addPageTSConfig('
 	mod.web_txtemplavoilaM1.javascript.file1 = ' . t3lib_extMgm::extRelPath('templavoila_framework') . 'core_templates/js/backend.js
 
 	#Include css file for backend styling
-	mod.web_txtemplavoilaM1.stylesheet = ' . t3lib_extMgm::extRelPath('templavoila_framework') . 'core_templates/css/' . $backendStylesheet . '
+	mod.web_txtemplavoilaM1.stylesheet = ' . t3lib_extMgm::extRelPath('templavoila_framework') . 'core_templates/css/backend.css
 ');
 
 $staticDataStructures = tx_templavoilaframework_lib::getStaticDataStructureArray(
