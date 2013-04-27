cef99bc70304ee52a6aeaa7d5aecc271eb24b4c2
Jeff Segars <jeff@busynoggin.com>
2013-04-26 23:04:52 -0500

    Move static resources to fit new naming conventions

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,10 +6,10 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStati
 // Update TemplaVoila with special stylesheet and Javascript
 t3lib_extMgm::addPageTSConfig('
 	#Include js file for backend styling
-	mod.web_txtemplavoilaM1.javascript.file1 = ' . t3lib_extMgm::extRelPath('templavoila_framework') . 'core_templates/js/backend.js
+	mod.web_txtemplavoilaM1.javascript.file1 = ' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('templavoila_framework') . 'Resources/Public/JavaScript/backend.js
 
 	#Include css file for backend styling
-	mod.web_txtemplavoilaM1.stylesheet = ' . t3lib_extMgm::extRelPath('templavoila_framework') . 'core_templates/css/backend.css
+	mod.web_txtemplavoilaM1.stylesheet = ' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('templavoila_framework') . 'Resources/Public/StyleSheets/backend.css
 ');
 
 $staticDataStructures = tx_templavoilaframework_lib::getStaticDataStructureArray(
