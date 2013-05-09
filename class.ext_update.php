23995dbaf1aceecac117827f1e7e6e93a91f4810
Jeff Segars <jeff@busynoggin.com>
2013-05-08 20:56:32 -0500

    [WIP] Update paths in database

--- class.ext_update.php
+++ class.ext_update.php
@@ -6,10 +6,20 @@ class ext_update {
 
 	protected $justImportedToPid = FALSE;
 
+	/**
+	 * Checks whether the update script should be active and allow access.
+	 *
+	 * @return boolean
+	 */
 	public function access() {
 		return TRUE;
 	}
 
+	/**
+	 * Main entry point for update form.
+	 *
+	 * @return string
+	 */
 	public function main() {
 		$content = array();
 
@@ -76,6 +86,11 @@ class ext_update {
 		return implode(chr(10), $content);
 	}
 
+	/**
+	 * Imports the template objects.
+	 *
+	 * @return string
+	 */
 	protected function importAction() {
 		$pid = intval(\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('pid'));
 		$result = $this->importTemplateObjects($pid);
@@ -100,11 +115,21 @@ class ext_update {
 		return $flashMessage->render() . $this->showFormAction();
 	}
 
+	/**
+	 * Checks if the given page ID has template objects.
+	 *
+	 * @return boolean
+	 */
 	protected function pageHasTemplateObjects($pid) {
 		$templateObjects = \TYPO3\CMS\Backend\Utility\BackendUtility::getRecordsByField('tx_templavoila_tmplobj', 'pid', $pid);
 		return count($templateObjects) > 0;
 	}
 
+	/**
+	 * Helper function for T3D import to a particular PID
+	 *
+	 * @return mixed
+	 */
 	protected function importTemplateObjects($pid) {
 		$templateObjectPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('templavoila_framework') . 'Resources/Private/Content/TemplateObjects.t3d';
 		$data = null;
@@ -123,12 +148,23 @@ class ext_update {
 		return $data;
 	}
 
+	/**
+	 * Writes extension configuration.
+	 *
+	 * @param array $xtensionConfiguration
+	 * @return void
+	 */
 	protected function writeExtensionConfiguration($extensionConfiguration) {
 		$configurationManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Configuration\\ConfigurationManager');
 		$configurationManager->setLocalConfigurationValueByPath('EXT/extConf/templavoila_framework', serialize($extensionConfiguration));
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::removeCacheFiles();
 	}
 
+	/**
+	 * Reads extension configuration
+	 *
+	 * @return mixed
+	 */
 	protected function readExtensionConfiguration() {
 		$extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['templavoila_framework']);
 		if (!$extensionConfiguration) {
@@ -136,6 +172,118 @@ class ext_update {
 		}
 
 		return $extensionConfiguration;
+	}
+
+	/**
+	 * Checks if the database needs an update
+	 *
+	 * @return boolean
+	 */
+	protected function databaseNeedsUpdate() {
+		return $this->pageDataStructureNeedsUpdate() || $this->contentDataStructureNeedsUpdate() || $this->templateObjectDataStructureNeedsUpdate() || $this->templateObjectFileRefNeedsUpdate();
+	}
+
+	/**
+	 * Performs necessary database updates for new skin paths.
+	 *
+	 * @return void
+	 */
+	public function updateDatabase() {
+		if ($this->pageDataStructureNeedsUpdate()) {
+			$this->updatePageDataStructure();
+		}
+
+		if ($this->contentDataStructureNeedsUpdate()) {
+			$this->updateContentDataStructure();
+		}
+
+		if ($this->templateObjectDataStructureNeedsUpdate()) {
+			$this->updateTemplateObjectDataStructure();
+		}
+
+		if ($this->templateObjectFileRefNeedsUpdate()) {
+			$this->updateTemplateObjectFileRef();
+		}
+	}
+
+	/**
+	 * Checks if the DS field on pages needs an update
+	 *
+	 * @return boolean
+	 */
+	protected function pageDataStructureNeedsUpdate() {
+		$statement = $GLOBALS['TYPO3_DB']->prepare_SELECTquery('COUNT(*)', 'pages', 'tx_templavoila_ds LIKE \'%:path%\'');
+		$statement->execute(array(':path' => 'EXT:templavoila_framework/core_templates/'));
+		$row = $statement->fetch(\TYPO3\CMS\Core\Database\PreparedStatement::FETCH_NUM);
+		$statement->free();
+
+		return $row[0] > 0;
+	}
+
+	/**
+	 * Updates the DS field on pages
+	 *
+	 * @return void
+	 */
+	protected function updatePageDataStructure() {
+		$oldPath = 'EXT:templavoila_framework/core_templates/datastructures/page/';
+		$newPath = 'EXT:templavoila_framework/Configuration/TemplaVoila/DataStructure/Pages/';
+
+		$fieldsValues = array(
+			'tx_templavoila_ds' => 'replace(\'tx_templavoila_ds\', \'' . $oldPath . '\', \'' . $newPath . '\')'
+		);
+		$GLOBALS['TYPO3_DB']->exec_UPDATEquery('pages', 'tx_templavoila_ds LIKE \'%' . $oldPath . '%\'', $fieldsValues);
+	}
+
+	/**
+	 * Check if the DS field on tt_content needs an update
+	 *
+	 * @return boolean
+	 */
+	protected function contentDataStructureNeedsUpdate() {
+		$statement = $GLOBALS['TYPO3_DB']->prepare_SELECTquery('COUNT(*)', 'tt_content', 'tx_templavoila_ds LIKE \'%:path%\'');
+		$statement->execute(array(':path' => 'EXT:templavoila_framework/core_templates/'));
+		$row = $statement->fetch(\TYPO3\CMS\Core\Database\PreparedStatement::FETCH_NUM);
+		$statement->free();
+
+		return $row[0] > 0;
+	}
+
+	/**
+	 * Updates the DS field for tt_content
+	 *
+	 * @return void
+	 */
+	protected function updateContentDataStructure() {
+
+	}
+
+	/**
+	 * Checks if the DS field on template objects needs an update
+	 *
+	 * @return boolean
+	 */
+	protected function templateObjectDataStructureNeedsUpdate() {
+		$statement = $GLOBALS['TYPO3_DB']->prepare_SELECTquery('COUNT(*)', 'tx_templavoila_tmplobj', 'fileref LIKE \'%:path%\'');
+		$statement->execute(array(':path' => 'EXT:templavoila_framework/core_templates/'));
+		$row = $statement->fetch(\TYPO3\CMS\Core\Database\PreparedStatement::FETCH_NUM);
+		$statement->free();
+
+		return $row[0] > 0;
+	}
+
+	/**
+	 * Checks if the fileref field on template objects needs an update
+	 *
+	 * @return boolean
+	 */
+	protected function templateObjectFileRefNeedsUpdate() {
+		$statement = $GLOBALS['TYPO3_DB']->prepare_SELECTquery('COUNT(*)', 'tx_templavoila_tmplobj', 'fileref LIKE \'%:path%\'');
+		$statement->execute(array(':path' => 'EXT:templavoila_framework/core_templates/'));
+		$row = $statement->fetch(\TYPO3\CMS\Core\Database\PreparedStatement::FETCH_NUM);
+		$statement->free();
+
+		return $row[0] > 0;
 	}
 
 }
